5cb0b45aaa59a676166f583cf606adb7172be8be
feh-franc0 <fernandofv1110@gmail.com>
2023-08-23 18:38:00 -0300

    Criando o contexto de navegação

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { View, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { Text } from 'react-native';
+
+import { Routes } from './src/routes';
+
 import { Loading } from '@components/Loading';
 import { THEME } from './src/theme';
-import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -16,7 +17,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
