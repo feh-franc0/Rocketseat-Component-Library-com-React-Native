ae6c5bad1eca1b7d6e6ce566c70112efbdde3e27
feh-franc0 <fernandofv1110@gmail.com>
2023-08-25 23:40:20 -0300

    Inputs de nome e e-mails

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,8 +1,10 @@
-import { ScreenHeader } from "@components/ScreenHeader";
-import { UserPhoto } from "@components/UserPhoto";
 import { Center, ScrollView, Skeleton, Text, VStack } from "native-base";
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Input } from "@components/Input";
 
 const PHOTO_SIZE = 33;
 
@@ -37,6 +39,17 @@ export function Profile() {
               Alterar foto
             </Text>
           </TouchableOpacity>
+
+          <Input 
+            bg="gray.600"
+            placeholder="Nome"
+          />
+
+          <Input 
+            bg="gray.600"
+            placeholder="E-mail"
+            isDisabled
+          />
         </Center>
       </ScrollView>
     </VStack>
