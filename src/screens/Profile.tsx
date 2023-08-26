48075a1176c49e96b1f03bd526586cbf3ca7a8d7
feh-franc0 <fernandofv1110@gmail.com>
2023-08-26 00:04:36 -0300

    Recap da Profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -15,7 +15,8 @@ export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
-      <ScrollView>
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
           {
             photoIsLoading
@@ -51,10 +52,8 @@ export function Profile() {
             placeholder="E-mail"
             isDisabled
           />
-        </Center>
 
-        <VStack px={10} mt={12} mb={9}>
-          <Heading color="gray.200" fontSize="md" mb={2}>
+          <Heading color="gray.200" fontSize="md" mb={2} alignSelf="flex-start" mt={12}>
             Alterar senha 
           </Heading>
 
@@ -80,7 +79,7 @@ export function Profile() {
             title="Atualizar"
             mt={4}
           />
-        </VStack>
+        </Center>
       </ScrollView>
     </VStack>
   )
