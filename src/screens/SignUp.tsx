a274c30b77afffd8a7d5e0d389ad635b0d171dbc
feh-franc0 <fernandofv1110@gmail.com>
2023-08-28 11:46:41 -0300

    Estilizando mensagens de validação

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -63,13 +63,10 @@ export function SignUp() {
                 placeholder="Nome" 
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.name?.message}
               />
             )}
           />
-
-          <Text color="white">
-            {errors.name?.message}
-          </Text>
 
           <Controller 
             control={control}
@@ -88,13 +85,10 @@ export function SignUp() {
                 autoCapitalize="none"
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.email?.message}
               />
             )}
           />
-
-          <Text color="white">
-            {errors.email?.message}
-          </Text>
 
           <Controller 
             control={control}
